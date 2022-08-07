03be229e88acece3231222418e41b5331321149b
Jatin Singh <63118687+sjatin050@users.noreply.github.com>
2022-08-07 22:18:30 +0530

    Time: 53 ms (23.30%), Space: 10.9 MB (59.94%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
+    // if I pass by reference the strings then T.C will become less
     int dp[1000+1][1001];
-    
-    int solve(string &str1,string &str2,int ind1,int ind2)
+    string str1,str2;
+    int solve(int ind1,int ind2)
     {
         if(ind1<=0 || ind2<=0)
             return 0;
@@ -11,15 +12,17 @@ public:
             return dp[ind1][ind2];
         }
         if(str1[ind1-1]==str2[ind2-1])
-        return dp[ind1][ind2]=1+solve(str1,str2,ind1-1,ind2-1);
+        return dp[ind1][ind2]=1+solve(ind1-1,ind2-1);
         else
         {
-            return dp[ind1][ind2]=max(solve(str1,str2,ind1-1,ind2),solve(str1,str2,ind1,ind2-1));
+            return dp[ind1][ind2]=max(solve(ind1-1,ind2),solve(ind1,ind2-1));
         }
     }
-    int longestCommonSubsequence(string str1, string str2) 
+    int longestCommonSubsequence(string st1, string st2) 
     {
         memset(dp,-1,sizeof(dp));
-        return solve(str1,str2,str1.size(),str2.size());
+        str1=st1;
+        str2=st2;
+        return solve(str1.size(),str2.size());
     }
 };
